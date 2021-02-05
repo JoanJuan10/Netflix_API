1520f1cfdb1781158f2bcc5bcb66e446fbaf2b4d
Jose <jmarinmalagon@gmail.com>
2021-02-05 01:51:10 +0100

    Primer update

---
+++ API_C06_ER_SQL/API_C06_ER_SQL/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_C06_ER_SQL.Models
+{
+    public partial class Cliente
+    {
+        public Cliente()
+        {
+            Videos = new HashSet<Videos>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int? Dni { get; set; }
+        public DateTime? Fecha { get; set; }
+
+        public virtual ICollection<Videos> Videos { get; set; }
+
+    }
+}
---
+++ API_C06_ER_SQL/API_C06_ER_SQL/Models/Videos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_C06_ER_SQL.Models
+{
+    public partial class Videos
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int? CliId { get; set; }
+
+        public virtual Cliente Cli { get; set; }
+    }
+}
